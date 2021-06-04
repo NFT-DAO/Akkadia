8c63a3d5e17c9976eea125d4b64ccc41f5650d9f
Noel Kwan <noelkwan1998@gmail.com>
2021-06-04 19:05:29 +0800

    Audit sendable.js

--- forkable/sendable.js
+++ forkable/sendable.js
@@ -16,7 +16,9 @@
  */
 import blockfrost from './blockfrost.js'
 
+// TODO Abstract to environment variable.
 const price = '2000000'; // Lovelace
+// TODO Remove if unused / Declare purpose if used.
 const minimum = 200000
 
 /**
@@ -24,8 +26,13 @@ const minimum = 200000
  * @param {string} sender The sender's public address.
  * @param {function} callback future callback for bd
  */
+// TODO Ensure full list is returned.
+//      Currently `getAddressHistory` retrieves only the first 100 transactions,
+//      due to endpoint parameter defaults.
+// TODO Refactor function logic, according to upstream comments (in `auto_rec_send.js`).
 const find_all_trx = (sender, callback=()=>{}) => {
   return blockfrost.getAddressHistory(sender).then((history) => {
+    // TODO Implement proper logging
     // console.log(history)
     if (history.data === undefined) {
       return []
@@ -35,12 +42,16 @@ const find_all_trx = (sender, callback=()=>{}) => {
   });
 };
 
+// TODO Rename function according to its intended purpose
+// TODO Refactor function logic, according to upstream comments (in `auto_rec_send.js`).
+// TODO Fix function interface and associated logic:
+//      all_tx, addresses both serve the same purpose of providing buyer addresses.
 const helper = (all_tx, sender, addresses) => {
   if (all_tx.length === 0) {return addresses}
   const tx = all_tx[0]
   return build_sendable(tx, sender).then((addr)=>{
     // console.log(addr)
-    if (addr !== '') {
+    if (addr !== '') { // TODO: Improve validation logic
       addresses.push(addr)
     }
     return helper(all_tx.slice(1), sender, addresses)
@@ -48,18 +59,27 @@ const helper = (all_tx, sender, addresses) => {
   })
 };
 
+// TODO Rename function according to intended purpose. E.g. send_NFTs etc...
 const build_sendable = (tx, sender) => {
   return blockfrost.getTrxIOInfo(tx).then((res) => {
     // INPUTS // OUTPUTS
     const inputs = res.data.inputs;
     const outputs = res.data.outputs;
+    // TODO Defend against case that inputs is null / undefined / length 0
     const potential_address = inputs[0].address
     // OUTPUTS: assume constant stake address
+
+    // TODO Abstract up validation logic.
+    //      It is both located at 1. and 2.
+    //      Move it into a function call.
+    // TODO `flag` is used to denote valid transactions.
+    //      This should be renamed appropriately.
     let flag = false;
     outputs.forEach(outs => {
       if (outs.address === sender) {
         const out_tokens = outs.amount
         out_tokens.forEach(tok => {
+          // TODO 1.
           if (tok.unit === 'lovelace' && tok.quantity === price) {
             flag = true;
           }
@@ -67,11 +87,16 @@ const build_sendable = (tx, sender) => {
       }
     })
     if (flag === true) {
+      // TODO 2.
       return confirm_single('', inputs, false, potential_address)
     } else {return ''}
   })
 }
 
+// TODO Document function purpose.
+//      This makes the required function interface apparent.
+// TODO Refactor function body into sequential validation steps.
+// TODO Document function parameters and their usage.
 const confirm_single = (status, inputs, breakout, potential_address) => {
   if (breakout) {
     // console.log('MULTIPLE STAKE')
